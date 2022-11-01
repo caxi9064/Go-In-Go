1f0fe489eb3958c73091a0771621a96545b38e6a
johnowagon <97626704+johnowagon@users.noreply.github.com>
2022-11-01 14:00:59 -0600

    Added grid creation

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main(){
-	GUI.GUI();
+	GUI.CreateGrid();
 }
--- pkg/GUI/GUI.go
+++ pkg/GUI/GUI.go
@@ -1,14 +1,33 @@
 package GUI
 
 import (
+	"image/color"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	//"fyne.io/fyne/v2/position"
 )
 
-func GUI() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
+func CreateGrid() {
+	/*
+	 * Will create and return the playing grid for our playing surface. 
+	 * Maybe we need to define a struct for our GUI elements; grid, pieces, pass button, etc.
+	 */
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Grid Layout")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
-	w.ShowAndRun()
+	grid := container.NewGridWithColumns(19)
+
+	for y := 0; y < 19; y++{
+		for x := 0; x < 19; x++{
+			bg := canvas.NewRectangle(color.Gray{0xE0})
+			
+			grid.Add(bg)
+		}
+	}
+	
+	myWindow.SetContent(grid)
+	myWindow.Resize(fyne.NewSize(700, 700))
+	myWindow.ShowAndRun()
 }
